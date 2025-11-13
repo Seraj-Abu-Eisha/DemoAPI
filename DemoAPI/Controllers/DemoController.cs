c25d5722c239567f35a2b8e0d342355a005ac53d
Seraj-Abu-Eisha <serajabueisha@gmail.com>
2025-11-13 19:50:48 +0200

    .

---
+++ DemoAPI/Controllers/DemoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace DemoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        [HttpGet]
+        public string SayHelloAPI()
+        {
+            return "hello world api";
+        }
+    }
+}
--- DemoAPI/obj/Debug/net9.0/DemoAPI.AssemblyInfo.cs
+++ DemoAPI/obj/Debug/net9.0/DemoAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c0aed8b8859ff96002377e71d904b03160cf85f")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
